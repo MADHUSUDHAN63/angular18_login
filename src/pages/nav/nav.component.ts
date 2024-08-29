908e7662b9dfda9c0cd1a3c6559214a49738cde8
madhusudhan <MADHUSUDHAN63>
2024-08-29 21:39:27 +0530

    Final Commit

--- src/pages/nav/nav.component.ts
+++ src/pages/nav/nav.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component ,inject} from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet,  } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink,RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  router=inject(Router);
 
+
+  logout(){
+    this.router.navigate(['login']);
+  }
 }
